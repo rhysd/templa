0c19d6b0e111ff156506f07bf992f823ccd4653f
rhysd <lin90162@yahoo.co.jp>
2014-01-05 10:07:35 +0900

    add get() helper for boost::variant

---
+++ src/helper/variant.hpp
@@ -0,0 +1,43 @@
+#if !defined TEMPLA_HELPER_VARIANT_HPP_INCLUDED
+#define      TEMPLA_HELPER_VARIANT_HPP_INCLUDED
+
+#include <boost/variant/variant.hpp>
+#include <boost/variant/static_visitor.hpp>
+#include <boost/variant/apply_visitor.hpp>
+#include <boost/optional.hpp>
+
+#include <utility>
+
+namespace templa {
+namespace variant {
+
+namespace detail {
+
+    template<class T>
+    struct static_getter : public boost::static_visitor<boost::optional<T>> {
+        typedef boost::optional<T> return_type;
+
+        return_type operator()(T const& val) const
+        {
+            return val;
+        }
+
+        template<class U>
+        return_type operator()(U const&) const
+        {
+            return boost::none;
+        }
+    };
+
+} // namespace detail
+
+template<class T, class... Args>
+inline boost::optional<T> get(boost::variant<Args...> const& v)
+{
+    return boost::apply_visitor(detail::static_getter<T>{}, v);
+}
+
+} // namespace variant
+} // namespace templa
+
+#endif    // TEMPLA_HELPER_VARIANT_HPP_INCLUDED

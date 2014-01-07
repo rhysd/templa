9b5377623637f67c41125c88320d107d59eb23a8
rhysd <lin90162@yahoo.co.jp>
2014-01-08 02:18:36 +0900

    dump AST for debug

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,10 +1,16 @@
 #include "compiler.hpp"
+#include "ast_dumper.hpp"
+
+#include <iostream>
 
 namespace templa {
 
 std::string compiler::compile(std::string const& code)
 {
-    parser.parse(code);
+    ast::ast a = parser.parse(code);
+
+    // TODO: Temporary
+    std::cout << ast::dump_ast(a) << std::endl;
     return "";
 }
 
